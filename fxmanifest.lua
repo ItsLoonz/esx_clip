9a8fff5e775d50350ff220b6d34971e422fd8c52
Loonz <104168918+ItsLoonz@users.noreply.github.com>
2022-04-21 22:53:32 +0200

    --Created fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'adamant'
+game 'gta5'
+
+
+description 'ESX Resource clip. Compatible with FiveM'
+author "ESX, Modified by Loonz"
+
+version '1.1.0'
+
+client_script 'Client.lua' 
+
+server_script 'Server.lua'
+dependencies 'es_extended'
